c526a984a7eac46c6d0ffd072154109f26339b27
Degreatjoe <jgreat770@gmail.com>
2024-06-04 23:53:27 +0100

    print preorder list

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - prints a binary tree
+ * using pre-order traversal
+ *
+ * @tree: the tree to print
+ * @func: pointer to a function call for each node
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);		
+}
