4fabaf8d13e24e2f18e0f864d9d9ee37c800f1b9
Degreatjoe <jgreat770@gmail.com>
2024-06-04 22:36:46 +0100

    adding a node to the left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,25 +15,31 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	/*check if parent is empty*/
 	if (parent == NULL)
-		return(NULL);
+		return (NULL);
 
-	/*first create a new node by utilizing our
-	previous newnode functuin*/
+	/**
+	 * first create a new node by utilizing our
+	 * previous newnode function
+	 */
 	Newnode = binary_tree_node(parent, value);
-	
+
 	/*check if the newnode creation was successful*/
 	if (Newnode == NULL)
-		return(NULL);
+		return (NULL);
 
 	/*check if the parent node already has a left child*/
 	if (parent->left != NULL)
 	{
-		/*sets the newnode left pointer to the current
-		left child of the parent*/
+		/**
+		 * sets the newnode left pointer to the current
+		 * left child of the parent
+		 */
 		Newnode->left = parent->left;
 
-		/*Updates the current left child's parent pointer
-		to the new node.*/
+		/**
+		 * Updates the current left child's parent pointer
+		 * to the new node
+		*/
 		parent->left->parent = Newnode;
 	}
 	/*else Sets the parent's left pointer to the new node*/
