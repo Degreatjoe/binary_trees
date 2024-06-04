d31c1884e49bd62bc8e404e29f75ae368c63f745
Degreatjoe <jgreat770@gmail.com>
2024-06-04 22:28:50 +0100

    adding a node to the left

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,15 +12,19 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	/*allocate memory for the node*/
 	binary_tree_t *NewNode = malloc(sizeof(binary_tree_t));
 
+	/*check if the memory allocation was successful*/
 	if (!NewNode)
 		return (NULL);
 
+	/*assign various attribute to their respective variables*/
 	NewNode->n = value;
 	NewNode->parent = parent;
 	NewNode->left = NULL;
 	NewNode->right = NULL;
 
+	/*return the pointer to the created node*/
 	return (NewNode);
 }
